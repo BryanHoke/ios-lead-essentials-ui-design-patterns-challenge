027aed60cb92ad9d60ea15d797d5981cf3f7a149
Bryan Hoke <zinth@icloud.com>
2021-09-01 18:04:31 -0500

    [MVP] Extract `FeedErrorViewModel` into its own file.

---
+++ MVP Module/Source/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedErrorViewModel.swift
+//  MVP
+//
+//  Created by Bryan Hoke on 9/1/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+	let message: String?
+}
